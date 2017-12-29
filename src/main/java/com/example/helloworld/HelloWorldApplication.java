e25917d1c62c92b89374b6912018092cfa0aed35
markiewb <benno.markiewicz@googlemail.com>
2017-12-29 17:15:02 +0100

    Add JavaDoc

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -7,6 +7,13 @@ import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
+/**
+ * <ul>
+ * <li>Start via <code>java -jar ./target/dropwizard.gettingstarted-1.0-SNAPSHOT.jar server hello-world.yml</code></li>
+ * <li>Open <a href="http://localhost:8080/hello-world?name=Benno">http://localhost:8080/hello-world?name=Benno</a></li>
+ * <p>
+ * </ul>
+ */
 public class HelloWorldApplication extends Application<HelloWorldConfiguration>
 {
     public static void main(String[] args) throws Exception
